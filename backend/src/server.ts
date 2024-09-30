79bd784472c49a1de46aa4d0aa104745d5385795
Dag <dagmara.szadkowska@wp.pl>
2024-09-30 11:09:33 +0200

    init: update server file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
-const app = express();
-const port = 3000;
+const app: express.Application = express();
+const port: number = 3000;
 
-app.get('/api/hello', (req, res) => {
+app.get('/api/hello', (req: Request, res: Response): void => {
   res.json({ message: 'Hello from backend!' });
 });
 
-app.listen(port, () => {
+app.listen(port, (): void => {
   console.log(`Backend listening at http://localhost:${port}`);
 });
