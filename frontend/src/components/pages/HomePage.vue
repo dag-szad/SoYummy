c157200b30682d0e2b8141c1edf18843d2e7c1a7
Dag <dagmara.szadkowska@wp.pl>
2024-10-01 10:59:40 +0200

    feat: add basic development pages

---
+++ frontend/src/components/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <img src="../../assets/icons/logo.svg" alt="Inactive logo" />
+    <h1>Welcome to the app!</h1>
+    <p>
+        This app offers more than just a collection of recipes - it is designed
+        to be your very own digital cookbook. You can easily save and retrieve
+        your own recipes at any time.
+    </p>
+    <router-link to="/register">
+        <button>Registration</button>
+    </router-link>
+    <router-link to="/login">
+        <button>Sign in</button>
+    </router-link>
+</template>
