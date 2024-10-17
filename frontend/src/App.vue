97a15a82108cc68525cac060c1de26edd519f1ee
Dag <dagmara.szadkowska@wp.pl>
2024-10-17 10:06:23 +0200

    feat: add store and update app file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useUserStore } from './store/index'
+
+const userStore = useUserStore()
+
+onMounted(() => {
+    userStore.loadUserData()
+})
+</script>
 
 <template>
     <router-view />
 </template>
-
-<style scoped></style>
