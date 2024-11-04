92c1ae36dc9dec4c9e77c9a94b955364f92d1556
Dag <dagmara.szadkowska@wp.pl>
2024-11-04 13:47:39 +0100

    feat: update search page

--- frontend/src/components/pages/Search.vue
+++ frontend/src/components/pages/Search.vue
@@ -1,14 +1,19 @@
 <template>
-    <div>
+    <div class="search-page">
         <page-title title="Search" />
-        <search-bar addon/>
+        <search-bar addon />
     </div>
 </template>
 
 <script setup lang="ts">
-import PageTitle from '../templates/PageTitle.vue';
-import SearchBar from '../templates/SearchBar.vue';
-</script> 
+import PageTitle from '../templates/PageTitle.vue'
+import SearchBar from '../templates/SearchBar.vue'
+</script>
 
 <style lang="scss" scoped>
+.search-page {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+}
 </style>
