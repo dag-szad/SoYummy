e4c3f56986001e2bbc014ada18eba3f8bbc0755a
Dag <124502546+DagmaraSzadkowska@users.noreply.github.com>
2024-11-04 10:03:23 +0100

    feat: add search page components

---
+++ frontend/src/components/pages/Search.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <page-title title="Search" />
+        <search-bar addon/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import PageTitle from '../templates/PageTitle.vue';
+import SearchBar from '../templates/SearchBar.vue';
+</script> 
+
+<style lang="scss" scoped>
+</style>
--- frontend/src/components/templates/SearchBar.vue
+++ frontend/src/components/templates/SearchBar.vue
@@ -1,6 +1,53 @@
 <template>
-    <div>
-        <input type="text" />
-        <button>Search</button>
+    <div class="search">
+        <input type="text" class="search__bar" />
+        <button class="search__button">Search</button>
+    </div>
+    <div class="addon" v-if="addon">
+        <h3>Search by:</h3>
+        <button class="addon__button" @click="toggleDropdown">
+            {{ selectedOption }}
+        </button>
+        <div class="addon__content" v-if="isDropdownOpen">
+            <button
+                class="addon__option"
+                v-for="opt in options"
+                :key="opt"
+                @click="selectOption(opt)"
+            >
+                {{ opt }}
+            </button>
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+    option: {
+        type: String,
+        default: 'Title',
+        required: true,
+    },
+    addon: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+const selectedOption = ref(props.option)
+const isDropdownOpen = ref(false)
+const options = ['Title', 'Ingredients']
+
+function selectOption(option: string) {
+    selectedOption.value = option
+    isDropdownOpen.value = false
+}
+
+function toggleDropdown() {
+    isDropdownOpen.value = !isDropdownOpen.value
+}
+</script>
+
+<style lang="scss" scoped></style>
