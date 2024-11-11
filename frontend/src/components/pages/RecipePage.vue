74e2cafead3549533f16d660ff3444915b7dacdf
Dag <dagmara.szadkowska@wp.pl>
2024-11-11 15:39:59 +0100

    feat: start working on ingredients component

--- frontend/src/components/pages/RecipePage.vue
+++ frontend/src/components/pages/RecipePage.vue
@@ -1,7 +1,13 @@
 <template>
     <recipe-banner />
+    <recipe-ing />
+    <recipe-prep />
 </template>
+
 <script setup lang="ts">
 import RecipeBanner from '../templates/RecipeBanner.vue'
+import RecipeIng from '../templates/RecipeIng.vue'
+import RecipePrep from '../templates/RecipePrep.vue'
 </script>
+
 <style lang="scss" scope></style>
--- frontend/src/components/templates/RecipeIng.vue
+++ frontend/src/components/templates/RecipeIng.vue
@@ -1,4 +1,62 @@
 <template>
+    <div class="ing">
+        <ul class="ing__titles">
+            <li>Ingredients</li>
+            <li>Number</li>
+            <li>Add to list</li>
+        </ul>
+        <ul>
+            <li>
+                <img src="" alt="" />
+                <h3>Title</h3>
+                <p>3 chopped</p>
+                <input type="checkbox" />
+            </li>
+        </ul>
+    </div>
 </template>
+
 <script setup lang="ts"></script>
-<style lang="scss" scope></style>
+
+<style lang="scss" scope>
+.ing {
+    margin: 200px auto 50px;
+    max-width: 1240px;
+
+    @media (min-width: 768px) {
+        margin: 115px auto 100px;
+    }
+
+    &__titles {
+        display: flex;
+        justify-content: space-between;
+        gap: 20px;
+
+        background-color: var(--main-accent);
+        border-radius: 8px;
+
+        color: var(--main-btn-txt);
+        font-size: 0.625rem;
+        font-weight: 600;
+
+        list-style: none;
+
+        padding: 12px 14px;
+
+        li:first-child {
+            flex-grow: 2;
+        }
+
+        @media (min-width: 768px) {
+            gap: 40px;
+            font-size: 1.125rem;
+            padding: 21px 32px;
+        }
+
+        @media (min-width: 768px) {
+            gap: 110px;
+            padding: 21px 40px;
+        }
+    }
+}
+</style>
