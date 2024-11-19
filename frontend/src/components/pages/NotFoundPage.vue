9218c9505870f575df4de82d30460a88d9f293f7
Dag <dagmara.szadkowska@wp.pl>
2024-11-19 08:34:48 +0100

    feat: update img srcset to new images

--- frontend/src/components/pages/NotFoundPage.vue
+++ frontend/src/components/pages/NotFoundPage.vue
@@ -5,14 +5,16 @@
             alt="404 page image"
             class="error__image"
             :srcset="`
-                    /src/assets/images/notFoundPage/404-mobile@1x.png 260w,
-                    /src/assets/images/notFoundPage/404-mobile@2x.png 520w,
+                    /src/assets/images/notFoundPage/404-mobile@1x.png 375w,
+                    /src/assets/images/notFoundPage/404-mobile@2x.png 750w,
                     /src/assets/images/notFoundPage/404-tablet@1x.png 500w,
                     /src/assets/images/notFoundPage/404-tablet@2x.png 1000w
+                    /src/assets/images/notFoundPage/404-desktop@1x.png 750w,
+                    /src/assets/images/notFoundPage/404-desktop@2x.png 1500w
                     `"
-            sizes="(max-width: 767px) 261px, 
-                (min-width: 768px) 500px,
-                100vw"
+            sizes="(max-width: 767px) 375px, 
+                (max-width: 1099px) 500px,
+                (min-width: 1100px) 750px"
         />
         <div class="error__info">
             <h1 class="error__title">We are sorry,</h1>
