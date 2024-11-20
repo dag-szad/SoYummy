673837259f19c5e5889b3c206ebe90fd58a78ea0
Dag <dagmara.szadkowska@wp.pl>
2024-11-20 09:14:22 +0100

    feat: update fav list router

--- backend/src/routes/favoriteListRouter.ts
+++ backend/src/routes/favoriteListRouter.ts
@@ -2,14 +2,18 @@ import express from 'express'
 import {
     addFavorite,
     deleteFavorite,
+    getFavorite,
 } from '../controllers/favoriteListController'
 
 const router = express.Router()
 
 // Dodawanie przepisu do ulubionych
-router.post('/:favListId/add/:recipeId', addFavorite)
+router.post('/:favListId/items', addFavorite)
 
 // Usuwanie przepisu z ulubionych
-router.delete('/:favListId/delete/:recipeId', deleteFavorite)
+router.delete('/:favListId/items/:recipeId', deleteFavorite)
+
+// Dodawanie przepisu do ulubionych
+router.get('/:favListId/items', getFavorite)
 
 export default router
