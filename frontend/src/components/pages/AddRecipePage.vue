3f1ab7b37e2e0e0d97bef6f91fceac19f9fcc2fd
Dag <dagmara.szadkowska@wp.pl>
2024-11-27 11:28:40 +0100

    feat: add base for add recipe pages

---
+++ frontend/src/components/pages/AddRecipePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <page-title title="Add recipe"/>
+
+</template>
+
+<script setup lang="ts">
+import PageTitle from '../templates/PageTitle.vue';
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/components/pages/MyRecipesPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <page-title title="My recipes" />
+</template>
+
+<script setup lang="ts">
+import PageTitle from '../templates/PageTitle.vue'
+</script>
+
+<style lang="scss" scoped></style>
